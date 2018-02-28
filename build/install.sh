4d7e0fb52a226320ef9367734ed0950f49cc1684
xiejieyi <xiejieyi2003@qq.com>
2018-02-28 15:34:51 +0800

    新增编译打包脚本

---
+++ build/install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+user=hobby_user
+
+# 检查用户是否存在
+if id $user &> /dev/null ;then 
+    # 存在，删除
+    userdel -r  hobby_user     
+fi
+
+# 检查docker是否已安装
+docker_version=`docker -v | grep version`
+if [ -z "$STRING" ]; then 
+    # 准备docker环境（暂时在线安装）离线安装未实施
+    apt-get update
+
+    #允许apt使用HTTPS的镜像源
+    apt-get install \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        software-properties-common
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+    apt-key fingerprint 0EBFCD88
+
+    add-apt-repository \
+       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) \
+       stable"
+
+    apt-get update
+    apt-get install docker-ce=1.13.1
+fi
+
+# 创建用户名和密码，并加入docker组
+groupadd docker
+useradd hobby_user -d /home/hobby_user -p "Admin123"
+sudo usermod -aG docker hobby_user
+
+cp -rf ./nginx /home/hobby_user/
+mkdir /home/hobby_user/mysql
+mkdir -p /home/hobby_user/mysql/logs /home/hobby_user/mysql/data
+cp -rf ./bin/start.sh /home/hobby_user/start.sh
+
--- build/package.sh
+++ build/package.sh
@@ -1,8 +1,66 @@
+#!/bin/bash
+
 # 前提条件，安装了git
 git clone https://github.com/xiejieyi-bear/interest_group.git
 
-# 安装npm
-apt install nodejs
-apt install npm
+work_path=$(dirname $(readlink -f "$0"))
+echo $work_path
+cd ..
+root_path=$(dirname $(readlink -f "$0"))
 
-npm config set registry https://registry.npm.taobao.org
+rm -rf release
+mkdir -p release
+
+# 打包client
+echo ******************1. client********************
+cd $root_path/client
+# 复制二进制文件
+cp $root_path/3rdparty/node_module.zip ./
+unzip node_module.zip
+npm run build:prod
+cp $root_path/client/dist $root_path/release
+
+echo ******************1. client end****************
+
+echo ******************2. build nginx image*********
+
+# 打包nginx docker 镜像
+cd $root_path/src/nginx
+
+cp -rf $root_path/client/dist ./
+docker build -t xiejieyi/nginx .
+
+echo ******************2. build nginx image end*****
+
+echo ******************3. build server image********
+
+cd $root_path/src/eureka_server
+mvn clean
+mvn package docker:build
+
+cd $root_path/src/auth
+mvn clean
+mvn package docker:build
+
+cd $root_path/src/gateway
+mvn clean
+mvn package docker:build
+
+echo ******************3. build server image end**********
+
+echo ******************4. package environment*************
+cd $root_path/release
+mkdir hobby_group
+cd hobby_group
+# 1.复制nginx文件
+mkdir nginx
+cp -rf $root_path/src/nginx/conf nginx/
+# 2.复制启动脚本
+mkdir bin
+cp $root_path/build/start.sh bin/
+# 3.安装脚本
+cp $root_path/build/install.sh ./
+
+echo ******************4. package environment end*********
+
+echo ******************package sueccess  *****************
