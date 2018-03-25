dffb18a210a07a67472aa859844111a80c14d786
xiejieyi <xiejieyi2003@qq.com>
2018-03-25 16:58:54 +0800

    修改TODO列表

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
-	NODE_ENV: '"production"',
-	ENV_CONFIG: '"prod"',
-	BASE_API: '"https://api-prod"'
+  NODE_ENV: '"production"',
+  ENV_CONFIG: '"prod"',
+  BASE_API: '"http://www.xiejieyi-bear.top:8080"'
 }
